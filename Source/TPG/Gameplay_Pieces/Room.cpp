29ebd253613170d48d8d791d7522a7b0edeeaaa7
VincentBaxterDerby <100309601@unimail.derby.ac.uk>
2025-10-11 23:54:11 +0100

    Defined room class

--- Source/TPG/Gameplay_Pieces/Room.cpp
+++ Source/TPG/Gameplay_Pieces/Room.cpp
@@ -9,6 +9,26 @@ ARoom::ARoom()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	RootComponent = RootScene;
+
+	Entrance = CreateDefaultSubobject<USceneComponent>(TEXT("Entrance"));
+	Entrance->SetupAttachment(RootComponent);
+
+	Exit = CreateDefaultSubobject<USceneComponent>(TEXT("Exit"));
+	Exit->SetupAttachment(RootComponent);
+
+	Structure = CreateDefaultSubobject<USceneComponent>(TEXT("Structure"));
+	Structure->SetupAttachment(RootComponent);
+
+	DoorDetector = CreateDefaultSubobject<UBoxComponent>(TEXT("DoorDetector"));
+	DoorDetector->OnComponentBeginOverlap.AddDynamic(this, &ARoom::OnOverlapBegin);
+	DoorDetector->SetupAttachment(RootComponent);
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(RootComponent);
+
+	bDoorOpen = false;
 }
 
 // Called when the game starts or when spawned
@@ -25,3 +45,11 @@ void ARoom::Tick(float DeltaTime)
 
 }
 
+void ARoom::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!bDoorOpen)
+	{
+		bDoorOpen = true;
+	}
+}
--- Source/TPG/Gameplay_Pieces/Room.h
+++ Source/TPG/Gameplay_Pieces/Room.h
@@ -38,8 +38,14 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "My Components|Meshes")
 	UStaticMeshComponent* Door;
 
+	bool bDoorOpen;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
